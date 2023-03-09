052b6283a17a03d38662871b2f5cb8f469853986
BattlerHaru <kronk.sdance@gmail.com>
2023-03-09 15:38:45 -0600

    se agregaron trycatch en varias funciones del controlador del usuario, se corrigio la creacion de nuevas categorias que impedia crear nuevas categorias

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -7,59 +7,84 @@ const getAllCategories = async (req = request, res = response) => {
 
   const {limit = 10, start = 0} = req.query;
 
-  const [total, categories] = await Promise.all([
-    CategoryModel.countDocuments(query),
-    CategoryModel.find(query)
-      .populate("user", "name")
-      .skip(Number(start))
-      .limit(Number(limit)),
-  ]);
+  try {
+    const [total, categories] = await Promise.all([
+      CategoryModel.countDocuments(query),
+      CategoryModel.find(query)
+        .populate("user", "name")
+        .skip(Number(start))
+        .limit(Number(limit)),
+    ]);
 
-  res.status(200).json({
-    msg: "Lista de categorías.",
-    total: `Total de categorías: ${total}.`,
-    categories,
-  });
+    res.status(200).json({
+      msg: "Lista de categorías.",
+      total: `Total de categorías: ${total}.`,
+      categories,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Ha ocurrido un error, hable con el administrador.",
+      error: error,
+    });
+  }
 };
 
 const getCategoryById = async (req = request, res = response) => {
   const {id} = req.params;
 
-  const categoryData = await CategoryModel.findById(id).populate(
-    "user",
-    "name"
-  );
+  try {
+    const categoryData = await CategoryModel.findById(id).populate(
+      "user",
+      "name"
+    );
 
-  res.status(200).json({
-    msg: "Categoría encontrada",
-    categoryData,
-  });
+    res.status(200).json({
+      msg: "Categoría encontrada",
+      categoryData,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Ha ocurrido un error, hable con el administrador.",
+      error: error,
+    });
+  }
 };
 
 const createCategory = async (req = request, res = response) => {
-  const newName = req.body.name.toUpperCase();
+  const newCategory = req.body.name.toUpperCase();
 
-  const categoryDB = await CategoryModel.findOne({newName});
+  try {
+    const categoryDB = await CategoryModel.findOne({name: newCategory});
 
-  if (categoryDB) {
-    return res.status(404).json({
-      msg: `La categoría: ${newName}, ya existe`,
+    if (categoryDB) {
+      return res.status(404).json({
+        msg: `La categoría: ${newCategory}, ya existe`,
+      });
+    }
+
+    const data = {
+      name: newCategory,
+      user: req.user._id,
+    };
+
+    const category = new CategoryModel(data);
+    console.log("Paso 4");
+
+    await category.save();
+
+    return res.status(201).json({
+      msg: "Categoría creada con éxito.",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Ha ocurrido un error, hable con el administrador.",
+      error: error,
     });
   }
-
-  const data = {
-    name: newName,
-    user: req.user._id,
-  };
-
-  const category = new Category(data);
-
-  await category.save();
-
-  res.status(201).json({
-    msg: "Categoría creada con éxito.",
-    category,
-  });
 };
 
 module.exports = {
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -9,32 +9,48 @@ const getUsers = async (req = request, res = response) => {
 
   const {limit = 5, start = 0} = req.query;
 
-  const [total, users] = await Promise.all([
-    UserModel.countDocuments(query),
-    UserModel.find(query).skip(Number(start)).limit(Number(limit)),
-  ]);
+  try {
+    const [total, users] = await Promise.all([
+      UserModel.countDocuments(query),
+      UserModel.find(query).skip(Number(start)).limit(Number(limit)),
+    ]);
 
-  res.status(200).json({
-    msg: "Lista de usuarios.",
-    total: `Total de usuarios: ${total}.`,
-    users,
-  });
+    res.status(200).json({
+      msg: "Lista de usuarios.",
+      total: `Total de usuarios: ${total}.`,
+      users,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Ha ocurrido un error, hable con el administrador.",
+      error: error,
+    });
+  }
 };
 
 const createUser = async (req = request, res = response) => {
   const {email, name, password, role, status} = req.body;
 
-  const user = new UserModel({name, email, password, role, status});
+  try {
+    const user = new UserModel({name, email, password, role, status});
 
-  user.email = email.toLowerCase();
-  user.password = hashPass(password);
+    user.email = email.toLowerCase();
+    user.password = hashPass(password);
 
-  await user.save();
+    await user.save();
 
-  res.status(201).json({
-    msg: `Usuario: ${name}, registrado con éxito.`,
-    user,
-  });
+    res.status(201).json({
+      msg: `Usuario: ${name}, registrado con éxito.`,
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Ha ocurrido un error, hable con el administrador.",
+      error: error,
+    });
+  }
 };
 
 const editUser = async (req = request, res = response) => {
@@ -45,26 +61,42 @@ const editUser = async (req = request, res = response) => {
     userData.password = hashPass(password);
   }
 
-  const user = await UserModel.findByIdAndUpdate(id, userData);
+  try {
+    const user = await UserModel.findByIdAndUpdate(id, userData);
 
-  res.status(200).json({
-    msg: "Usuario actualizado con éxito.",
-    user,
-  });
+    res.status(200).json({
+      msg: "Usuario actualizado con éxito.",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Ha ocurrido un error, hable con el administrador.",
+      error: error,
+    });
+  }
 };
 
 const deleteUser = async (req = request, res = response) => {
   const {id} = req.params;
 
-  // Delete is not recommend
-  // const user = await UserModel.findByIdAndDelete(id);
+  try {
+    // Delete is not recommend
+    // const user = await UserModel.findByIdAndDelete(id);
 
-  // Update status
-  const user = await UserModel.findByIdAndUpdate(id, {status: false});
+    // Update status
+    const user = await UserModel.findByIdAndUpdate(id, {status: false});
 
-  res.status(200).json({
-    msg: `Usuario: ${user.name}, dado de baja con éxito.`,
-  });
+    res.status(200).json({
+      msg: `Usuario: ${user.name}, dado de baja con éxito.`,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Ha ocurrido un error, hable con el administrador.",
+      error: error,
+    });
+  }
 };
 
 const usersPatch = (req = request, res = response) => {
